61520c99bea90c7bef302a993bf96be4a2fe0440
kajyuuen <kajyuuen@gmail.com>
2018-10-30 17:28:46 +0900

    Update env_set.sh

--- env_set.sh
+++ env_set.sh
@@ -1,7 +1,8 @@
-# default
+# Default
 export HOST_PORT=`expr 10000 + $UID`
 export HOST_DIRECTORY=~/
 export CONTAINER_DIRECTORY=/tmp
 
-export CONTAINER_NAME=kajyuuen-pytorch-con
-export IMAGE_NAME=kajyuuen-pytorch
+# User settings
+export CONTAINER_NAME=
+export IMAGE_NAME=
